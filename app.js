08617137abc8a88275cae082f1d95f1027a5c4d5
James Aikens <jaikens19@hotmail.com>
2021-01-20 16:17:12 -0600

    Completed Phase 3

--- app.js
+++ app.js
@@ -2,9 +2,22 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'pug')
+
 app.get('/', (req, res) => {
   res.send('Hello from Express!');
 });
+
+app.get(/^xyz$/  , (req, res) => {
+  res.send("That's all I wrote.");
+});
+
+app.all('*', (req, res) => {
+   const method = req.method
+   const path = req.path
+   let random_number = Math.floor(Math.random() * 10)
+   res.render('layout', { method, path, random_number })
+})
 
 const port = 8081;
 
