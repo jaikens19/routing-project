d376a13c6a73ac94d19ed461730a49de6807389a
Yassine Cherradi <yassine.cherradi@yahoo.com>
2021-01-20 23:56:11 +0100

    project complete

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const route = require('./route');
 const app = express();
 
 app.set('view engine', 'pug')
@@ -8,15 +8,27 @@ app.get('/', (req, res) => {
   res.send('Hello from Express!');
 });
 
-app.get(/^xyz$/  , (req, res) => {
+app.get('/about/foo', (req, res) => {
+  res.status(404);
+  res.send('Error 404. Not Found')
+});
+
+app.get(/xyz$/, (req, res) => {
   res.send("That's all I wrote.");
 });
 
+app.get('/capital-letters/:id', (req, res) => {
+  res.send(req.params.id.toUpperCase());
+});
+
+app.use('/margot', route);
+app.use('/margeaux', route);
+
 app.all('*', (req, res) => {
-   const method = req.method
-   const path = req.path
-   let random_number = Math.floor(Math.random() * 10)
-   res.render('layout', { method, path, random_number })
+  const method = req.method
+  const path = req.path
+  let random_number = Math.floor(Math.random() * 10)
+  res.render('layout', { method, path, random_number })
 })
 
 const port = 8081;
